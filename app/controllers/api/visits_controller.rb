f2069c10f2f7993b57bd72839d5eee881f9be8af
Lucas D'Avila <lucas@lucasdavi.la>
2016-01-22 14:24:09 -0200

    Added visits api.

---
+++ app/controllers/api/visits_controller.rb
@@ -0,0 +1,43 @@
+class Api::VisitsController < ApplicationController
+  def create
+    @visit = Visit.new permited_params
+    @visit.visitor = current_visitor
+
+    if @visit.save
+      render json: @visit.to_json, status: 201
+    else
+      render json: { errors: @visit.errors }.to_json, status: 400
+    end
+  end
+
+  protected
+
+  def permited_params
+    params.permit :url, :datetime
+  end
+
+  def current_visitor
+    create_visitor if current_session_id.blank? || _current_visitor.blank?
+    _current_visitor
+  end
+
+  def create_visitor
+    Visitor.create! session_id: set_session_id
+  end
+
+  def set_session_id
+    cookies.permanent[:session_id] = random_session_id
+  end
+
+  def random_session_id
+    SecureRandom.hex 32
+  end
+
+  def current_session_id
+    cookies[:session_id]
+  end
+
+  def _current_visitor
+    @current_visitor ||= Visitor.find_by_session_id current_session_id
+  end
+end
---
+++ spec/requests/api/visits_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::Visits', type: :request do
+  describe 'POST /visit' do
+    let(:data) { { url: 'http://foobar.com/', datetime: Time.now, visitor_id: 10000 } }
+    let(:last_visit) { Visit.last }
+    let(:new_visitor) { Visitor.find_by_session_id cookies['session_id'] }
+
+    context 'when it is from a new visitor' do
+      before do
+        cookies.delete :session_id
+      end
+
+      it 'creates a visit and visitor' do
+        expect { post api_visits_path, data }.to change(Visit, :count).by(1)
+        expect(response).to have_http_status 201
+        expect(response.content_type).to eq 'application/json'
+        expect(response.body).to eq last_visit.to_json
+        expect(response_json['visitor_id']).to eq new_visitor.id
+      end
+    end
+
+    context 'when it is from a returning visitor' do
+      before do
+        cookies['session_id'] = 'foo_session_id'
+      end
+
+      let!(:returning_visitor) { Visitor.create! session_id: 'foo_session_id' }
+
+      it 'creates a visit to the returning visitor' do
+        expect { post api_visits_path, data }.to change(Visit, :count).by(1)
+        expect(response).to have_http_status 201
+        expect(response.content_type).to eq 'application/json'
+        expect(response.body).to eq last_visit.to_json
+        expect(response_json['visitor_id']).to eq returning_visitor.id
+      end
+    end
+
+    context 'when there is a validation error' do
+      let(:expected_errors) { { 'url' => ["can't be blank"] } }
+
+      it 'returns the errors messages' do
+        post api_visits_path
+
+        expect(response).to have_http_status 400
+        expect(response_json['errors']).to include expected_errors
+      end
+    end
+  end
+end
